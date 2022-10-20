abef4dcc7a9bc2b1553f1f6322d86b64ac36367c
nighoggDatatype <hosherman1965@gmail.com>
2022-10-21 04:44:16 +0800

    Add untested sentence embed

--- cnn.py
+++ cnn.py
@@ -1,14 +1,32 @@
 # Possible guide: https://medium.com/dair-ai/deep-learning-for-nlp-an-overview-of-recent-trends-d0d8f40a776d
+# Note: Might change from cnn to regular nn for ease of implementation
+#       But might use cnn anyway for potentially powerful model
+
 
 import argparse
 
 import numpy as np
 import pandas as pd
+import gensim
+
+
+# Code inspired by: https://towardsdatascience.com/super-easy-way-to-get-sentence-embedding-using-fasttext-in-python-a70f34ac5b7c
+wordEmbeddingsModel = gensim.downloader.load('fasttext-wiki-news-subwords-300')
+def sentenceVec(sentence):
+    i = 0
+    acc = np.zeros(300)
+    for word in sentence.split():
+        i+=1
+        acc += wordEmbeddingsModel.wv[word] 
+    return acc/i #TODO: Test this
 
 def cnn(csv):
     df = pd.read_csv(csv)
-    #TODO
-    pass
+    df["comment_embbed"] = df.apply(lambda row: sentenceVec(row.comment), axis=1)
+    #TODO: See about adding sentence embbed for parent and post
+    
+
+    #TODO: Finish this
 
 def main(args):
     assert args.csv, "Please specify --csv"
@@ -19,7 +37,7 @@ def get_arguments():
     parser.add_argument('--csv', help='path to the dataset file')
     return parser.parse_args()
 
-# Usage: python svm.py --csv data/cleaned_comments.csv
+# Usage: python cnn.py --csv data/cleaned_comments.csv
 if __name__ == "__main__":
     args = get_arguments()
     main(args)
