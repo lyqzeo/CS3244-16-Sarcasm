205a426b20b122c7a9ab997e98d7662b9e2af7ac
skythefire <minganntan@gmail.com>
2022-10-21 14:43:14 +0800

    Add svm model

--- svm.py
+++ svm.py
@@ -5,29 +5,97 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-from sklearn import svm
-from sklearn.svm import LinearSVC
-from sklearn.model_selection import train_test_split
+import gensim
+from gensim.models import Word2Vec
 
-def svm(csv):
+from sklearn import metrics
+from sklearn import svm
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
+
+def svm(csv): # best so far
     df = pd.read_csv(csv)
-    # remove non english comments?
 
     # all columns except "labels" column
     X = df.loc[:, df.columns != "label"]
+    x_comment = X.comment
 
-    # (true labels) labels: 1 = sarcastic, 0 = not sarcastic
-    y = df["label"].to_numpy()
+    y = df["label"]
 
-    # split the dataset into training and test datasets
-    X_train, X_test, y_train, y_test = train_test_split(X, y)
+    vect = CountVectorizer(strip_accents='unicode', max_df=0.70)
+    tf_trans = TfidfTransformer()
 
-    C = 1.0 # SVM regularization parameter
-    # svc = svm.SVC(kernel='linear', C=1,gamma=0).fit(X, y)
-    LinearSVM = LinearSVC().fit(X_train, y_train)
-    print("training set score: %f" % LinearSVM.score(X_train, y_train))
-    print("test set score: %f" % LinearSVM.score(X_test, y_test))
+    # create the SVM model
+    svm_model = SGDClassifier(penalty="elasticnet", random_state=42, n_jobs=-1)
+
+    pipeline = Pipeline([
+        ('vect', vect),
+        ('tftrans', tf_trans),
+        ('model', svm_model)
+    ])
     
+    param_grid = {
+        'vect__ngram_range': [(1, 1), (1, 2), (1, 3)],
+        'vect__max_features': (5000, 15000, 30000),
+        'model__l1_ratio': (0.0, 0.15, 0.40, 0.60, 0.85, 1.0)
+    }
+
+    grid_svm = GridSearchCV(pipeline, param_grid, scoring="accuracy", cv=3, n_jobs=-1)
+    grid_svm.fit(x_comment, y)
+
+    print(grid_svm.best_score_)
+    for param_name in sorted(param_grid.keys()):
+        print("%s: %r" % (param_name, grid_svm.best_params_[param_name]))
+
+
+def svm2(csv):
+    df = pd.read_csv(csv)
+
+    x = df.comment
+    y = df.label
+
+    X_train, X_test, y_train, y_test = train_test_split(x, y)
+
+    count_vect = CountVectorizer(strip_accents='unicode', max_df=0.70)
+    c = count_vect.fit(X_train)
+    vect = TfidfTransformer()
+
+    count_X_train = c.transform(X_train)
+    count_X_test = c.transform(X_test)
+    
+    vec_X_train = vect.transform(count_X_train)
+    vec_X_test = vect.transform(count_X_test)
+
+    svc = LinearSVC(penalty='l1', C=0.55, fit_intercept=False, dual=False, tol=1e-10, max_iter=100000)
+
+    svc.fit(vec_X_train, y_train)
+    y_pred = svc.predict(vec_X_test)
+    print(classification_report(y_pred, y_test))
+
+def svm3(csv): # using word2vec / sent2vec
+    df = pd.read_csv(csv)
+
+    # all columns except "labels" column
+    # X = df.loc[:, df.columns != "label"]
+
+    x = df.comment
+    y = df.label
+
+    # TODO: implement
+    model = gensim.models.Word2Vec(x,
+                 vector_size=100
+                 # Size is the length of our vector.
+                )
+
+
+    # X_train, X_test, y_train, y_test = train_test_split(x, y)
+
 
 def main(args):
     assert args.csv, "Please specify --csv"
