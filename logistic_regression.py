6a0b7d07d67db6dee3af337c189bd4379197d668
alinaleehx <alinaleehx@gmail.com>
2022-11-25 16:18:29 +0800

    Update Log Regression

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import time
+import json
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
@@ -9,7 +10,7 @@ from matplotlib import pyplot as plt
 
 # Set up own cleaned_data_full
 
-def run_model(model, X_train, y_train, X_test, y_test, verbose=True):
+def run_model(model, X_train, y_train, X_test, y_test, name, verbose=True):
     t0=time.time()
     if verbose == False:
         model.fit(X_train,y_train, verbose=0)
@@ -24,13 +25,9 @@ def run_model(model, X_train, y_train, X_test, y_test, verbose=True):
     print("Time taken = {}".format(time_taken))
     print(classification_report(y_test,y_pred,digits=5))
     plot_confusion_matrix(model, X_test, y_test,cmap=plt.cm.pink, normalize = 'all')
-    plot_roc_curve(model, X_test, y_test)                     
+    plot_roc_curve(model, X_test, y_test, name=name)                     
     
     return model, accuracy, roc_auc, time_taken
-
-df = pd.read_csv('data/cleaned_comments.csv')
-X = df.loc[:, df.columns != "label"]
-x_train, x_test, y_train, y_test = train_test_split(X, df['label'], random_state=42)
 
 # Remove stopwords
 import nltk
@@ -55,20 +52,20 @@ def logit_tfidf(csv):
     x_train.comment = x_train.comment.apply(lambda x: removeStopWords(x))
     
     # Model - standard
-    params_lr = {'penalty': 'elasticnet', 'l1_ratio':0.5, 'solver': 'saga'}
+    #params_lr = {'penalty': 'elasticnet', 'l1_ratio':0.5, 'solver': 'saga'}
+    params_lr = {'solver': 'liblinear'}
     model_lr = LogisticRegression(**params_lr)
     
     # Tf-idf (fit on x_train)
-    vectorizer = TfidfVectorizer(use_idf=True, ngram_range=(1,3), min_df=0.01, max_df=0.9)
+    vectorizer = TfidfVectorizer(use_idf=True, ngram_range=(1,2))
     vectorizer.fit(x_train['comment'])
     x_train = vectorizer.transform(x_train['comment'])
     x_test = vectorizer.transform(x_test['comment'])
 
     
-    model_lr, accuracy_lr, roc_auc_lr, tt_lr = run_model(model_lr, x_train, y_train, x_test, y_test)
+    model_lr, accuracy_lr, roc_auc_lr, tt_lr = run_model(model_lr, x_train, y_train, x_test, y_test, "tf-idf")
     return model_lr, accuracy_lr, roc_auc_lr, tt_lr
 
-#logit_tfidf("data/cleaned_comments.csv")
 # Scalar features logit
 ### Normalize Features
 from sklearn.preprocessing import StandardScaler
@@ -79,23 +76,7 @@ def normalize_scalar(x_train, x_test):
     x_test.loc[:, scalar_features] = scaler.fit_transform(x_test[scalar_features])
     return x_train.loc[:, scalar_features], x_test.loc[:, scalar_features]
 
-def logit_scalar(csv):
-    # Split data
-    df = pd.read_csv(csv)
-    X = df.loc[:, df.columns != "label"]
-    x_train, x_test, y_train, y_test = train_test_split(X, df['label'], random_state=42)
-    
-    # Model - standard
-    params_lr = {'penalty': 'elasticnet', 'l1_ratio':0.5, 'solver': 'saga'}
-    model_lr = LogisticRegression(**params_lr)
-    
-    # Normalize scalar
-    x_train, x_test = normalize_scalar(x_train, x_test)
-    model_lr, accuracy_lr, roc_auc_lr, tt_lr = run_model(model_lr, x_train, y_train, x_test, y_test)
-    return model_lr, accuracy_lr, roc_auc_lr, tt_lr
-
 # tf-idf combined
-
 def logit_combined(csv):
     # split data and remove stopwords
     df = pd.read_csv(csv)
@@ -108,7 +89,7 @@ def logit_combined(csv):
     model_lr = LogisticRegression(**params_lr)
         
     # Tf-idf (fit on x_train)
-    vectorizer = TfidfVectorizer(use_idf=True, ngram_range=(1,3), min_df=0.008, max_df=0.8)
+    vectorizer = TfidfVectorizer(use_idf=True, ngram_range=(1,2))
     vectorizer.fit(x_train['comment'])
     
     tf_idf_train = pd.DataFrame(vectorizer.transform(x_train['comment']).toarray(), 
@@ -124,31 +105,32 @@ def logit_combined(csv):
     test = pd.concat([x_test.loc[:, scalar_features], tf_idf_test], axis=1)
     
         
-    model_lr, accuracy_lr, roc_auc_lr, tt_lr = run_model(model_lr, train, y_train, test, y_test)
+    model_lr, accuracy_lr, roc_auc_lr, tt_lr = run_model(model_lr, train, y_train, test, y_test, "Combined")
 
+def logit_tfidf_limited(csv):
+    # split data and remove stopwords
+    df = pd.read_csv(csv)
+    X = df.loc[:, df.columns != "label"]
+    x_train, x_test, y_train, y_test = train_test_split(X, df['label'], random_state=42)
+    x_train.comment = x_train.comment.apply(lambda x: removeStopWords(x))
+    
+    # Model - standard
+    params_lr = {'solver': 'liblinear'}
+    model_lr = LogisticRegression(**params_lr)
+    
+    # Tf-idf (fit on x_train)
+    vectorizer = TfidfVectorizer(use_idf=True, ngram_range=(1,2), min_df=0.008, max_df=0.8)
+    vectorizer.fit(x_train['comment'])
+    x_train = vectorizer.transform(x_train['comment'])
+    x_test = vectorizer.transform(x_test['comment'])
 
-print("COMBINEd")
-logit_tfidf("data/cleaned_comments.csv")
-print("SCALAR")
-logit_scalar("data/cleaned_comments.csv")
+    
+    model_lr, accuracy_lr, roc_auc_lr, tt_lr = run_model(model_lr, x_train, y_train, x_test, y_test, "tf-idf limited")
+    return model_lr, accuracy_lr, roc_auc_lr, tt_lr
+
 print("TF_IDF")
-logit_combined("data/cleaned_comments.csv")
-
-
-#Count vectorizer for bag of words
-cv=CountVectorizer(min_df=0,max_df=1,binary=False,ngram_range=(1,3))
-cv_train = x_train.copy()
-cv_train['comment'] =cv.fit_transform(x_train['comment']).todense()
-cv_test =cv.transform(x_test['comment'])
-
-#model_lr, accuracy_lr, roc_auc_lr, tt_lr = run_model(model_lr, cv_train, y_train, cv_test, y_test)
-
-# Feature selection
-# from sklearn.feature_selection import SelectKBest, chi2
-# modified_data = pd.DataFrame(r_scaler.transform(train), columns=train.columns)
-# X = modified_data.loc[:,modified_data.columns!='label']
-# y = modified_data[['label']]
-# selector = SelectKBest(chi2, k=10)
-# selector.fit(X, y)
-# X_new = selector.transform(X)
-# print(X.columns[selector.get_support(indices=True)])
+logit_tfidf("data/cleaned_comments.csv")
+# print("TF_IDF_lcombined")
+# logit_combined("data/cleaned_comments.csv")
+print("TF_IDF_limited")
+logit_tfidf_limited("data/cleaned_comments.csv")
